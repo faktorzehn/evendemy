0e97379b72cd927535b052bd5f690a6e9f42647c
Thomas Wruss <thomas.wruss@gmail.com>
2018-03-19 21:54:30 +0100

    fixing test setup, fixing some tests, removing some tests

--- webapp/src/app/pages/error/error.component.spec.ts
+++ webapp/src/app/pages/error/error.component.spec.ts
@@ -1,14 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorComponent } from './error.component';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Client } from '../../middleware/client';
+import { HttpClientModule } from '@angular/common/http';
+import { ConfigModule, ConfigService } from '@ngx-config/core';
+import { StoreModule } from '@ngrx/store';
+import { ConfigLoader } from '@ngx-config/core';
+import { ConfigStaticLoader } from '@ngx-config/core';
+
+
+export function configFactory(): ConfigLoader {
+  return new ConfigStaticLoader({
+    backend_url: ''
+  });
+}
 
 describe('ErrorComponent', () => {
   let component: ErrorComponent;
   let fixture: ComponentFixture<ErrorComponent>;
 
   beforeEach(async(() => {
+    let configStub = {
+      getSettings: function(){
+        return { backend_url: ''}
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ ErrorComponent ]
+      declarations: [ ErrorComponent, MenuComponent ],
+      imports: [RouterTestingModule.withRoutes([]), StoreModule.forRoot({ }),  HttpClientModule],
+      providers: [Client, {provide: ConfigService, useValue: configStub }]
     })
     .compileComponents();
   }));
