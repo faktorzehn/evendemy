be8484fd0d7305c8f46bb24a8de00aba61cab27d
Thomas Wruss <thomas.wruss@gmail.com>
2018-05-11 16:15:06 +0200

    default picture for users

--- webapp/src/app/components/user-image/user-image.component.ts
+++ webapp/src/app/components/user-image/user-image.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ConfigService } from '@ngx-config/core';
 import { Client } from '../../middleware/client';
+import { User } from '../../model/user';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../appState';
 
 @Component({
   selector: 'app-user-image',
@@ -10,13 +13,33 @@ import { Client } from '../../middleware/client';
 export class UserImageComponent implements OnInit {
 
   @Input()
-  username = this.client.getLoggedInUsername();
+  username: string;
+  @Input()
+  width: number;
+  @Input()
+  height: number;
 
+  error = false;
   folder = this.config.getSettings().user_image_folder;
+  users: User[];
+  user: User;
 
-  constructor( private config: ConfigService, private client: Client) { }
+  constructor( private config: ConfigService, private client: Client, private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.select('users').subscribe( res => this.users = res );
+    this.username = this.client.getLoggedInUsername();
+    this.client.getUserByUsername(this.username).subscribe((u: User) => this.user = u);
   }
+
+  get initials() {
+    let result = '';
+    if (this.user) {
+      result += this.user.firstname[0];
+      result += this.user.lastname[0];
+    }
+    return result;
+  }
+
 
 }
