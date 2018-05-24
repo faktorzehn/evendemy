ee12df3c2967e8bce5a9b0991dba764e8097c5f7
Thomas Wruss <thomas.wruss@gmail.com>
2018-05-24 19:57:02 +0200

    meetingsRoutes validates if it is allowed to access data (restricted by new option), user-info displays if user has restricted his/her view

--- server/routes/meetingsRoutes.js
+++ server/routes/meetingsRoutes.js
@@ -1,6 +1,7 @@
 module.exports = function (server, config, production_mode) {
 
     var meetingService = require('../services/meetingService');
+    var userService = require('../services/userService');
 
     server.get('/meetings', function (req, res, next) {
         meetingService.getMeetings(req.params).then(function (meetings) {
@@ -14,11 +15,19 @@ module.exports = function (server, config, production_mode) {
 
     server.get('/meetings/attendee/:username', function (req, res, next) {
         if (req.params.username !== undefined) {
-            meetingService.getMeetingsForUser(req.params.username).then(function (meetings) {
-                res.send(meetings);
-            }, function (err) {
-                res.send(500, { error: err });
-            });
+            userService.getUserByUsername(req.params.username)
+            .then(user => {
+                if(req.params.username===req.user.uid){
+                    return user.username;
+                }
+                if(user && user.options && user.options.summary_of_meetings_visible === true){
+                    return user.username;
+                }
+                throw Error('Not allowed');
+            })
+            .then(meetingService.getMeetingsForUser)
+            .then( meetings => res.send(meetings))
+            .catch( err => res.send(500, { error: err })); 
         } else {
             res.send(500, { error: 'no username specified' });
             return;
@@ -27,11 +36,19 @@ module.exports = function (server, config, production_mode) {
 
     server.get('/meetings/author/:username', function (req, res, next) {
         if (req.params.username !== undefined) {
-            meetingService.getMeetingsFromAuthor(req.params.username).then(function (meetings) {
-                res.send(meetings);
-            }, function (err) {
-                res.send(500, { error: err });
-            });
+            userService.getUserByUsername(req.params.username)
+            .then(user => {
+                if(req.params.username===req.user.uid){
+                    return user.username;
+                }
+                if(user && user.options && user.options.summary_of_meetings_visible === true){
+                    return user.username;
+                }
+                throw Error('Not allowed');
+            })
+            .then( meetingService.getMeetingsFromAuthor)
+            .then( meetings => res.send(meetings))
+            .catch( err => res.send(500, { error: err })); 
         } else {
             res.send(500, { error: 'no username specified' });
             return;
