9d5f4568699a801c92a6c0c8035d4d52a745bca0
Thomas Wruss <thomas.wruss@gmail.com>
2018-06-13 19:42:23 +0200

    new evendemy logo!

--- webapp/src/app/pages/login/login.component.ts
+++ webapp/src/app/pages/login/login.component.ts
@@ -6,7 +6,7 @@ import { UsersService } from '../../services/users.service';
 @Component({
   selector: 'evendemy-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
