5e53a7c5a20f1eb0d2b4abf7faf606c9bea7284b
void toni <j.sztavi@protonmail.com>
2023-08-01 16:32:37 +0200

    Fixed a typo

--- backend/src/core/middleware/ui.middleware.ts
+++ backend/src/core/middleware/ui.middleware.ts
@@ -1,10 +1,10 @@
 
 import { NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 import * as path from 'path'
 
 export class UIMiddleWare implements NestMiddleware {
-    use(_req,res,_next) {
-            res.sendFile("idk.html") // Somehow send the webapps index.html
-        }
+    use(_req: Request,res: Response,_next:NextFunction) {
+            res.sendFile("idk.html"); // Somehow send the webapps index.html
     }
 }
