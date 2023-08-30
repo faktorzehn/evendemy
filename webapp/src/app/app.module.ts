d411b76bc2194f7afc9d61463cd2d1656bc42df9
void toni <j.sztavi@protonmail.com>
2023-08-30 09:41:34 +0200

    Fixed needing to log in to view images from the api with pipes.

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -50,6 +50,7 @@ import { MeetingCardComponent } from './components/meeting-card/meeting-card.com
 import { ConfirmDialogContentComponent } from './components/dialog/confirm-dialog-content/confirm-dialog-content.component';
 import { TranslocoRootModule } from './transloco-root.module';
 import { HammerModule } from '@angular/platform-browser';
+import { ProtectedImagePipe } from './pipes/protected-image.pipe';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -100,7 +101,8 @@ const appRoutes: Routes = [
     EditableInputComponent,
     UserInfoComponent,
     DialogComponent,
-    ConfirmDialogContentComponent
+    ConfirmDialogContentComponent,
+    ProtectedImagePipe,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -123,7 +125,7 @@ const appRoutes: Routes = [
     AuthenticationService,
     TagsService,
     ConfigService,
-    { provide: APP_INITIALIZER, useFactory: (config: ConfigService<any>) => config.load(), deps: [ConfigService], multi: true},
+    { provide: APP_INITIALIZER, useFactory: (config: ConfigService<any>) => config.load(), deps: [ConfigService, HttpClient], multi: true},
     [...interceptors]
   ],
   bootstrap: [AppComponent]
